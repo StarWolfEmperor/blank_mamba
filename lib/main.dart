63601083557ee76403292fb368b0d7cfa318c745
Star Wolf Emperor <folorunshoyusuf8@gmail.com>
2023-03-24 14:14:54 +0100

    updated background color settings

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Black Mamba',
       theme: ThemeData(
-        // This is the theme of your application.
+          // This is the theme of your application.
 
-        primarySwatch: Colors.blue,
-      ),
+          ),
       home: const MyHomePage(title: 'Black Mamba'),
     );
   }
@@ -48,107 +48,127 @@ class _MyHomePageState extends State<MyHomePage> {
     // This method is rerun every time setState is called, for instance as done
     // I.E ONCE THE WIDGETS ARE INVOKED, THE VALUE HERE IS ACTIVATED
     return Scaffold(
-      backgroundColor: Colors.black54,
+      //backgroundColor: Colors.black54,
       appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: Colors.transparent,
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Colors.black,
+                Colors.grey,
+              ],
+            ),
+          ),
+        ),
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         //APPBAR GOES HERE
-        title: Text(widget.title),
-        backgroundColor: Colors.black38,
       ),
-      body: SafeArea(
-        child: Center(
-          child: Row(children: [
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Text(
-                    'Blvck Mamba',
-                    textAlign: TextAlign.center,
-                    softWrap: true,
-                    style: TextStyle(
-                      fontFamily: 'Roboto',
-                      fontSize: 100.0,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 50.0,
-                    width: 150.0,
-                    child: Divider(),
-                  ),
-                  Text(
-                    'Black Lifestyle',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontFamily: 'Roboto',
-                      fontSize: 10.0,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(
-                    width: 200.0,
-                    height: 50.0,
-                    child: Text(
-                      'Aliqua excepteur esse do laborum et sunt qui eiusmod. Dolor ut pariatur mollit qui sint ad. Eu ullamco mollit incididunt velit anim commodo est consequat consequat culpa eiusmod. Tempor tempor elit consectetur aliqua. In ipsum esse proident adipisicing est eu nisi esse. Tempor quis pariatur deserunt fugiat ea mollit minim reprehenderit voluptate proident nostrud. Id nisi enim aliquip ad mollit laboris sunt pariatur velit laborum voluptate et officia.',
-                      textAlign: TextAlign.center,
-                      softWrap: true,
-                      style: TextStyle(
-                        fontFamily: 'Roboto',
-                        color: Colors.white,
-                        fontSize: 10.0,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Expanded(
-              child: Column(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [Colors.black, Colors.grey],
+          ),
+        ),
+        child: SafeArea(
+          child: Center(
+            child: Row(children: [
+              Expanded(
+                child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset(
-                      'images/coffee.png',
-                      //height: 600.0,
-                      fit: BoxFit.contain,
-                    ),
-                  ]),
-            ),
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: const [
-                  Text(
-                    'The Feels',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontFamily: 'Roboto',
-                      fontSize: 20.0,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  SizedBox(
-                    width: 200.0,
-                    height: 50.0,
-                    child: Text(
-                      'Aliqua excepteur esse do laborum et sunt qui eiusmod. Dolor ut pariatur mollit qui sint ad. Eu ullamco mollit incididunt velit anim commodo est consequat consequat culpa eiusmod. Tempor tempor elit consectetur aliqua. In ipsum esse proident adipisicing est eu nisi esse. Tempor quis pariatur deserunt fugiat ea mollit minim reprehenderit voluptate proident nostrud. Id nisi enim aliquip ad mollit laboris sunt pariatur velit laborum voluptate et officia.',
+                  children: const [
+                    Text(
+                      'Blvck Mamba',
                       textAlign: TextAlign.center,
                       softWrap: true,
                       style: TextStyle(
                         fontFamily: 'Roboto',
+                        fontSize: 100.0,
                         color: Colors.white,
-                        fontSize: 5.0,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                  ),
-                ],
+                    SizedBox(
+                      height: 50.0,
+                      width: 150.0,
+                      child: Divider(),
+                    ),
+                    Text(
+                      'Black Lifestyle',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontFamily: 'Roboto',
+                        fontSize: 10.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 200.0,
+                      height: 50.0,
+                      child: Text(
+                        'Aliqua excepteur esse do laborum et sunt qui eiusmod. Dolor ut pariatur mollit qui sint ad. Eu ullamco mollit incididunt velit anim commodo est consequat consequat culpa eiusmod. Tempor tempor elit consectetur aliqua. In ipsum esse proident adipisicing est eu nisi esse. Tempor quis pariatur deserunt fugiat ea mollit minim reprehenderit voluptate proident nostrud. Id nisi enim aliquip ad mollit laboris sunt pariatur velit laborum voluptate et officia.',
+                        textAlign: TextAlign.center,
+                        softWrap: true,
+                        style: TextStyle(
+                          fontFamily: 'Roboto',
+                          color: Colors.white,
+                          fontSize: 10.0,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ]),
+              Expanded(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        'images/coffee.png',
+                        height: 600.0,
+                      ),
+                    ]),
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
+                    Text(
+                      'The Feels',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontFamily: 'Roboto',
+                        fontSize: 20.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 200.0,
+                      height: 50.0,
+                      child: Text(
+                        'Aliqua excepteur esse do laborum et sunt qui eiusmod. Dolor ut pariatur mollit qui sint ad. Eu ullamco mollit incididunt velit anim commodo est consequat consequat culpa eiusmod. Tempor tempor elit consectetur aliqua. In ipsum esse proident adipisicing est eu nisi esse. Tempor quis pariatur deserunt fugiat ea mollit minim reprehenderit voluptate proident nostrud. Id nisi enim aliquip ad mollit laboris sunt pariatur velit laborum voluptate et officia.',
+                        textAlign: TextAlign.center,
+                        softWrap: true,
+                        style: TextStyle(
+                          fontFamily: 'Roboto',
+                          color: Colors.white,
+                          fontSize: 5.0,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ]),
+          ),
         ),
       ),
     );
